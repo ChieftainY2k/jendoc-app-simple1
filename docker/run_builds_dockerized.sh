e4d5e8e2c39528736d30f03b7771a80f01891c84
Tomasz <tomasz.rozdzynski@codete.co>
2016-08-12 11:24:29 +0200

    remove images after tasks

--- docker/run_builds_dockerized.sh
+++ docker/run_builds_dockerized.sh
@@ -7,21 +7,24 @@
 BUILD_DIR=/tmp/build/
 
 #Random namespace
-NAMESPACE=build-`date +"%Y%m%d%H%M%S"`-`shuf -i 1000-9000 -n 1`
+NAMESPACE=builds`date +"%Y%m%d%H%M%S"``shuf -i 1000-9000 -n 1`
 
 echo "[$NAMESPACE] Cleaning up containers..."
 docker-compose -p $NAMESPACE stop
 docker-compose -p $NAMESPACE rm -f
 
 echo "[$NAMESPACE] Building containers..."
-docker-compose -p $NAMESPACE build builds
+docker-compose -p $NAMESPACE build builder
 echo "[$NAMESPACE] Running containers with docker-compose (BUILD_DIR = $BUILD_DIR)..."
-docker-compose -p $NAMESPACE run -e BUILD_DIR=$BUILD_DIR builds scripts/run_builds.sh
+docker-compose -p $NAMESPACE run -e BUILD_DIR=$BUILD_DIR builder scripts/run_builds.sh
 EXITCODE=$?
 
 echo "[$NAMESPACE] Cleaning up containers..."
 docker-compose -p $NAMESPACE stop
 docker-compose -p $NAMESPACE rm -f
+
+echo "[$NAMESPACE] Cleaning up images..."
+docker rmi -f $(docker images | grep $NAMESPACE | awk '{ print $3 }')
 
 echo "[$NAMESPACE] Exit code is $EXITCODE"
 exit $EXITCODE
--- docker/run_tests_dockerized.sh
+++ docker/run_tests_dockerized.sh
@@ -5,21 +5,24 @@
 #
 
 #Random namespace
-NAMESPACE=test-`date +"%Y%m%d%H%M%S"`-`shuf -i 1000-9000 -n 1`
+NAMESPACE=tests`date +"%Y%m%d%H%M%S"``shuf -i 1000-9000 -n 1`
 
 echo "[$NAMESPACE] Cleaning up containers..."
 docker-compose -p $NAMESPACE stop
 docker-compose -p $NAMESPACE rm -f
 
 echo "[$NAMESPACE] Building containers..."
-docker-compose -p $NAMESPACE build tests
+docker-compose -p $NAMESPACE build tester
 echo "[$NAMESPACE] Running containers with docker-compose..."
-docker-compose -p $NAMESPACE run -e REPORTS_DIR="/tmp/test-reports/" tests scripts/run_tests.sh
+docker-compose -p $NAMESPACE run -e REPORTS_DIR="/tmp/test-reports/" tester scripts/run_tests.sh
 EXITCODE=$?
 
 echo "[$NAMESPACE] Cleaning up containers..."
 docker-compose -p $NAMESPACE stop
 docker-compose -p $NAMESPACE rm -f
+
+echo "[$NAMESPACE] Cleaning up images..."
+docker rmi -f $(docker images | grep $NAMESPACE | awk '{ print $3 }')
 
 echo "[$NAMESPACE] Exit code is $EXITCODE"
 exit $EXITCODE
